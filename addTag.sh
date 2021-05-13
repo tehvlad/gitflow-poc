78097109b532a9747c487a8fbdc969dd751ac373
Vladimir Bazan <vladimir.bazan@wizeline.com>
2021-05-13 13:54:19 -0500

    main 0.1.0-main.28 13/05/2021 13:54:16

--- addTag.sh
+++ addTag.sh
@@ -6,7 +6,7 @@ SEMVER=$(dotnet-gitversion | jq ."SemVer" | sed 's/"//g')
 NV=$(dotnet-gitversion | jq ."NuGetVersion" | sed 's/"//g')
 Branch=$(git branch --show-current)
 echo -e "$dt - $SEMVER - $NV - $Branch" | cat - stack.log > temp && mv temp stack.log
-tail stack.log
+head stack.log
 git add .
 git commit -m "$Branch $SEMVER $dt"
 git push
